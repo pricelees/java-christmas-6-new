0ebd474b87d181032cb555bedffe3492fe10c788
sangdol <priceelees@gmail.com>
2023-12-05 21:11:09 +0900

    test(ChristmasDiscountStrategy): 12월 1일부터 31일까지 크리스마스 디데이 할인 금액에 대한 테스트

---
+++ src/test/java/christmas/service/discount/strategy/ChristmasDiscountStrategyTest.java
@@ -0,0 +1,39 @@
+package christmas.service.discount.strategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import christmas.constant.Menu;
+import christmas.domain.Customer;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ChristmasDiscountStrategyTest {
+    @DisplayName("정확한 힐은 금액 반환 확인")
+    @ParameterizedTest(name = "12월 {0}일 방문시 {1}원 할인 확인")
+    @MethodSource("provideArguments")
+    void calculateDiscountAmount(int dayOfMonth, int expectedDiscountAmount) {
+        Customer customer = Customer.valueOf(
+                dayOfMonth, Map.of(Menu.CHRISTMAS_PASTA, 1) // 메뉴는 임의 지정
+        );
+        DiscountStrategy discountStrategy = new ChristmasDiscountStrategy();
+        assertThat(discountStrategy.calculateDiscountAmount(customer))
+                .isEqualTo(expectedDiscountAmount);
+
+    }
+
+    static Stream<Arguments> provideArguments() {
+        return IntStream.rangeClosed(1, 31)
+                .mapToObj(dayOfMonth -> {
+                    if (dayOfMonth <= 25) {
+                        return arguments(dayOfMonth, 1000 + 100 * (dayOfMonth - 1));
+                    }
+                    return arguments(dayOfMonth, 0);
+                });
+    }
+}
